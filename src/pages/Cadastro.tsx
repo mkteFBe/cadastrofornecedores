83b416cb22c7b8aaca13617fd4cfcbeafc47fb21
mkteFBe <marketing@filtrosbrasil.com.br>
2026-06-29 10:07:44 -0300

    Delete src directory

--- src/pages/Cadastro.tsx
+++
@@ -1,6 +0,0 @@
-import { FornecedorForm } from "@/components/fornecedor/FornecedorForm";
-import { Topbar } from "@/components/landing/Topbar";
-import { Header } from "@/components/landing/Header";
-import { Footer } from "@/components/landing/Footer";
-const Cadastro = () => (<div className="min-h-screen flex flex-col"><Topbar/><Header/><main className="flex-1 py-8 px-4" style={{background:"var(--fb-light-gray)"}}><FornecedorForm/></main><Footer/></div>);
-export default Cadastro;
